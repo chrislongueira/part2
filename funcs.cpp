e4172e6756faf9d09d221075581937bace44ba7c
chrislongueira <christopher.longueira66@myhunter.cuny.edu>
2018-03-29 10:01:19 -0400

    add

--- funcs.cpp
+++ funcs.cpp
@@ -10,17 +10,43 @@ Assignment: Lab 8
 using namespace std;
 
 string removeLeadingSpaces(string line){
-
+  string result = "";
+  for (int i = 0; i < line.size(); i++){
+    if (!isspace(line[i])){
+      result += line.substr(i);
+    }
+  }
+  return result;
 }
 
 int countChar(string line, char c){
+  int result = 0;
+  for (int i = 0; i < line.size(); i++){
+    char pre = (char) line[i];
+    if (curr == c){
+      result += 1;
+    }
+  }
+  return result;
+}
+
+string unindent(string line){
+  return removeLeadingSpaces(line);
 
 }
 
-int indent(){
-
-}
-
-int unindent(){
-
+string indent(){
+  string line;
+  string result = "";
+  int count = 0;
+  while (getline(cin,line)){
+    line = unindent(line);
+    count -= countChar(line, '}');
+    for (int i = 0; i < count; i++){
+      result += '\t';
+    }
+    result += line + "\n";
+    count += countChar(line, '{');
+  }
+  return result;
 }
---
+++ funcs.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cctype>
+using std::string;
+
+string removeLeadingSpaces(string line);
+
+int countChar(string line, char c);
