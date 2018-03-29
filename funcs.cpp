557ce69523d3fdade67b818adeaad3ba8554f6d7
chrislongueira <christopher.longueira66@myhunter.cuny.edu>
2018-03-29 08:40:04 -0400

    copied over lab 8

---
+++ funcs.cpp
@@ -0,0 +1,26 @@
+/*
+Author: Christopher Longueira
+Course: CSCI-136
+Instructor: Professor Mike Zamansky
+Assignment: Lab 8
+*/
+
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+string removeLeadingSpaces(string line){
+
+}
+
+int countChar(string line, char c){
+
+}
+
+int indent(){
+
+}
+
+int unindent(){
+
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << unindent("bad-code.cpp") << endl;
+  cout << indent("bad-code.cpp") << endl;
+
+  return 0;
+}
